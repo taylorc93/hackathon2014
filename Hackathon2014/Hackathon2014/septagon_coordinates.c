f9f508d28c03744a4c7c16e976ccc5c1118542fb
ThisIsTheCheeseMachine <intrinsicaudio@gmail.com>
2014-10-24 21:44:45 -0400

    Function to compute septagon coordinates

---
+++ Hackathon2014/Hackathon2014/septagon_coordinates.c
@@ -0,0 +1,42 @@
+//
+//  septagon_coordinates.c
+//  Hackathon2014
+//
+//  Created by Chris Penny on 10/24/14.
+//  Copyright (c) 2014 Intrinsic Audio. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int **septagon_coordinates(int r, int center)
+{
+    double theta[7] = { 0, 0.897597901025655, 1.795195802051310,
+        2.692793703076966, 3.590391604102621,
+        4.487989505128276, 5.385587406153931 };
+    
+    int *x = malloc(sizeof(int) * 7);
+    int *y = malloc(sizeof(int) * 7);
+    
+    for(int i = 0; i < 7; i++)
+    {
+        x[i] = (int)((-1 * r) * sin(theta[i]));
+        y[i] = (int)((-1 * r) * cos(theta[i]));
+    }
+    
+    int **coordinates = malloc(sizeof(int*) * 2);
+    coordinates[0] = x;
+    coordinates[1] = y;
+    
+    
+    for(int j = 0; j < 2; j++)
+    {
+        for(int i = 0; i < 7; i++)
+        {
+            coordinates[j][i] = coordinates[j][i] + center;
+        }
+    }
+    
+    return coordinates;
+}
